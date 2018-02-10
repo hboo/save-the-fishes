afc1bec1f41b240bdb5e98b175e84879bd63d0ed
Sarah Herr <sarah.herr@hotmail.com>
2018-02-10 14:38:03 -0500

    Some UI cleanup

--- browser/components/Species.js
+++ browser/components/Species.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import LearnMore from './LearnMore'
+// import LearnMore from './LearnMore'
 
 export default (props) =>
   <div className='species-container'>
     <p className='species-text'>{props.species}</p>
-    <LearnMore />
+    {/* <LearnMore /> */}
   </div>
