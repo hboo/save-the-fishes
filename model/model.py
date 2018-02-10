7b76182c8098510e170d9136600f8d05b64e903c
hboo <hyun.y.boo@mail.mcgill.ca>
2018-02-10 14:52:19 -0500

    properly freeze base model layers, initial predict.py

--- model/model.py
+++ model/model.py
@@ -11,22 +11,22 @@ import numpy as np
 from glob import glob
 import h5py 
 
-
-def build_model():
+def build_model(num_classes):
 	# Use VGG16 model trained on Image Net and finetune the top layer
 	base_model = applications.VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 	print('Model loaded')
-	import pdb;pdb.set_trace()
-	#for layer in base_model:
+	#import pdb;pdb.set_trace()
+	for layer in base_model.layers:
 		#Freeze imagenet trained layers
-	#	layer.trainable = False
+		layer.trainable = False
 	last = base_model.output
 	top_model = Flatten()(last)
 	top_model = Dense(256, activation='relu')(top_model)
-	top_model = Dense(1, activation='sigmoid')(top_model)
+	top_model = Dense(num_classes, activation='sigmoid')(top_model)
 
 	model = Model(base_model.input, top_model)
 
+	model.summary()
 	return model
 
 if __name__ == '__main__':
--- model/train.py
+++ model/train.py
@@ -1,3 +1,4 @@
+import os
 from sklearn.datasets import load_files
 from keras import applications
 from keras.utils import np_utils
@@ -14,23 +15,35 @@ import PIL
 
 from model import *
 
+fish_names = None
+PATH_TO_TRAIN = '../fishImages_mock/train/'
+PATH_TO_VALIDATE = '../fishImages_mock/valid/'
+
+def count_num_classes(path):
+	files = folders = 0
+	for _, dirnames, filenames in os.walk(path):
+	    files += len(filenames)
+	    folders += len(dirnames)
+	return folders
+
 def path_to_tensor(img_path):
-    # loads RGB image as PIL.Image.Image type
-    img = image.load_img(img_path, target_size=(224, 224))
-    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
-    x = image.img_to_array(img)
-    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
-    return np.expand_dims(x, axis=0)
+	# loads RGB image as PIL.Image.Image type
+	img = image.load_img(img_path, target_size=(224, 224))
+	# convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+	x = image.img_to_array(img)
+	# convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+	return np.expand_dims(x, axis=0)
 
 def paths_to_tensor(img_paths):
-    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
-    return np.vstack(list_of_tensors)
+	list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+	return np.vstack(list_of_tensors)
 
 def load_dataset(path):
-	NUM_FAMILIES = 1 # change to number of subfolders
+	NUM_FAMILIES = count_num_classes(PATH_TO_TRAIN)
 	data = load_files(path)
 	fish_files = np.array(data['filenames'])
 	fish_targets = np_utils.to_categorical(np.array(data['target']), NUM_FAMILIES)
+
 	return fish_files, fish_targets
 
 def augment_dataset():
@@ -38,18 +51,19 @@ def augment_dataset():
 	pass
 
 def train_model():
+	num_classes = count_num_classes(PATH_TO_TRAIN)
 	# load the train, test, and validation datasets
-	train_files, train_targets = load_dataset('../fishImages_mock/train')
-	valid_files, valid_targets = load_dataset('../fishImages_mock/valid')
-	#import pdb;pdb.set_trace()
+	train_files, train_targets = load_dataset(PATH_TO_TRAIN)
+	valid_files, valid_targets = load_dataset(PATH_TO_VALIDATE)
+	import pdb;pdb.set_trace()
 
 	# convert images to 4D tensor
 	train_tensors = paths_to_tensor(train_files).astype('float32')/255
 	valid_tensors = paths_to_tensor(valid_files).astype('float32')/255
 
 	# get the model architecture
-	model = build_model()
-	model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+	model = build_model(num_classes)
+	model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 	
 	# fit with a checkpointer which saves the best performing model under filepath
 	checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.from_scratch.hdf5', 
